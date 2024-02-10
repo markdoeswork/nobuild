b698f2d42de1b111eb63d9964aef6bb9e0cdd37b
Mark <mark.rosenberg@powerhrg.com>
2024-02-10 14:10:56 -0500

    Use component

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 import React from "react"
 import ReactDOM from "react-dom"
+import hello from "components/hello_world"
 
 // Connects to data-controller="react"
 export default class extends Controller {
   connect() {
     console.log("testmark")
 
-    const e = React.createElement
-    const root = ReactDOM.createRoot(document.getElementById("root"))
-    root.render(e("h1", null, "HelloWorld"), root)
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(React.createElement(hello), root);
   }
 }
