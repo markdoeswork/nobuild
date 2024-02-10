e77d83eef642022c56ffc18cbd41d82c26b260d4
Mark <mark.rosenberg@powerhrg.com>
2024-02-10 13:35:21 -0500

    Use stimulus

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,14 +1,14 @@
-import { Controller } from "@hotwired/stimulus";
-import { render } from "react-dom"
-import h from "components/hello_world"
+import { Controller } from "@hotwired/stimulus"
+import React from "react"
+import ReactDOM from "react-dom"
 
 // Connects to data-controller="react"
 export default class extends Controller {
   connect() {
-    console.log("testmark");
+    console.log("testmark")
 
-    // const e = React.createElement
-    // const root = ReactDOM.createRoot(document.getElementById("root"));
-    // root.render(e(HelloWorld), root);
+    const e = React.createElement
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(e("h1", null, "HelloWorld"), root)
   }
 }
